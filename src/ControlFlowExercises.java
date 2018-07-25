ae5ae9c5e13e8d7121267e85c62316f5f21ae08d
Amos Davis <amos.davis367@gmail.com>
2018-07-25 09:58:09 -0500

    java_Table question still in progress

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,25 +1,32 @@
+import java.util.Scanner;
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
+        int maxColumn = 3;
+        double maxRow = 15;
+//        Scanner sc = new Scanner(System.in);
 
-        String Fizz = "Fizz";
-        String Buzz = "Buzz";
+//
+       System.out.println();
 
-        for (int number = 1; number <= 100; number++) {
-            if (number % 3 == 0 && number % 5 == 0) {
-                System.out.println(Fizz + Buzz);
+       for (int wholeNumber = 1; wholeNumber <= maxColumn; wholeNumber++) {
+           System.out.printf("Number    | Squared     | Cubed     \n");
+       }
 
-                    continue;
+        for (int wholeNumber = 1; wholeNumber <= maxColumn; wholeNumber++) {
+           System.out.printf("%15d", wholeNumber);
+//       }
+//        System.out.printf("Your words: %s %s %s", userWord1, userWord2, userWord3);
 
-            } else if (number % 5 == 0) {
-                    System.out.println(Buzz);
-                    continue;
-
-            } else if (number % 3 == 0) {
-                System.out.println(Fizz);
-                 continue;
-             }
-                System.out.println(number);
-            }
-
-    }
+//        System.out.println("What number would you like to go up to?");
+//        userNumber = sc.nextInt();
+//        sc.nextLine();
+//
+//        System.out.println("Here is your table!");
+//
+//
+//        System.out.println("Do you want to keep going?");
+//        userInput = sc.next();
+        }
+   }
 }
