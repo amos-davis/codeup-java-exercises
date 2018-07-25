454e1bc4cf9d1477b322fdf1a00703fcfdf2efdf
Amos Davis <amos.davis367@gmail.com>
2018-07-24 21:45:38 -0500

    completed Exercise2_Java_FizzBuzz-challenge___Positioning is the Key_Must place and statement first, in order for program to run all 3 labels

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,43 +1,25 @@
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        byte doubling2s = 0;
-        do {
-            System.out.println(doubling2s + 2);
-            doubling2s += 2;
-        } while( doubling2s <= 98);
 
-//        ------This is a "for" loop for the above----
-//        for (byte doubling2s = 0; doubling2s <= 98;doubling2s +=2) {
-//            System.out.println(doubling2s + 2);
-//        }
-//
-//
-//
-//
-        byte decrementingByFive = 105;
-        do {
-            System.out.println(decrementingByFive - 5);
-            decrementingByFive -= 5;
-        } while( decrementingByFive >= -5);
-//
-////                ------This is a "for" loop for the above----
-//        for (byte decrementingByFive = 105; decrementingByFive >= -5;decrementingByFive -= 5) {
-//            System.out.println(decrementingByFive - 5);
-//        }
-//
-//
-//
-//
-////        -----Note: for some reason, it does not like "int". Had to do "long" or "double".------
-        long squaredNum = 2;
-        do {
-            System.out.println(squaredNum);
-            squaredNum *= squaredNum;
-        } while( squaredNum < 1000000);
-//
-////        ------This is a "for" loop for the above----
-//        for (long squaredNum = 2; squaredNum < 1000000;squaredNum *= i) {
-//            System.out.println(squaredNum);
-//        }
+        String Fizz = "Fizz";
+        String Buzz = "Buzz";
+
+        for (int number = 1; number <= 100; number++) {
+            if (number % 3 == 0 && number % 5 == 0) {
+                System.out.println(Fizz + Buzz);
+
+                    continue;
+
+            } else if (number % 5 == 0) {
+                    System.out.println(Buzz);
+                    continue;
+
+            } else if (number % 3 == 0) {
+                System.out.println(Fizz);
+                 continue;
+             }
+                System.out.println(number);
+            }
+
     }
 }
