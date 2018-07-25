475cf0c5d1a08ec1f1b522ff826905602b4fee3c
Amos Davis <amos.davis367@gmail.com>
2018-07-25 15:19:46 -0500

    still complete, but added feature for user input

--- src/Bob.java
+++ src/Bob.java
@@ -1,15 +1,23 @@
+import java.util.Scanner;
+
+
 public class Bob {
     public static void main(String[] args) {
-        String converseWithBob = "what about this,";
+        System.out.println("Say something to Bob:  ");
+        Scanner input = new Scanner(System.in);
+
+
+        String converseWithBob = input.nextLine();
 
         if (converseWithBob.endsWith("?")) {
             System.out.println("Sure.");
         } else if (converseWithBob.endsWith("!")) {
             System.out.println("Whoa, chill out!");
-        } else if (converseWithBob.equals(" ")) {
+        } else if (converseWithBob.isEmpty()) {
             System.out.println("Fine. Be that way!");
         } else {
             System.out.println("Whatever.");
         }
     }
 }
+
