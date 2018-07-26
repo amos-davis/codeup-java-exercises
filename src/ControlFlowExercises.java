83acb59af30c916553d90f110cfbc938bcc70e41
Amos Davis <amos.davis367@gmail.com>
2018-07-25 21:16:57 -0500

    completed Java Loops exercise with Table and user prompt

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,12 +2,12 @@ import java.util.Scanner;
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
-//        int maxRows = 15; ***(Change this number to enter static # of rows)***
-//
-        Scanner sc = new Scanner(System.in);
-        boolean userAnswer = true;
+//        int maxRows = 15; ***(Adjust this number to enter static # of rows)***
 
-//        do {
+        Scanner sc = new Scanner(System.in);
+        boolean userContinues = true;
+
+        do {
 
             System.out.println("What number would you like to go up to (from 0 - 15)?");
             int userNumber = sc.nextInt();
@@ -26,22 +26,22 @@ public class ControlFlowExercises {
 //            }
 //
 //                ----The Below "for" loop is the same table, but Refactored----
- for (int wholeNumber = 1; wholeNumber <= userNumber; wholeNumber++) {
-            System.out.printf("%-7d", wholeNumber);
-     System.out.print("|");
-           System.out.printf("%-10d", wholeNumber * wholeNumber);
-     System.out.print("|");
-     System.out.printf("%-9d\n", wholeNumber * wholeNumber * wholeNumber);
-       }
+                for (int wholeNumber = 1; wholeNumber <= userNumber; wholeNumber++) {
+                    System.out.printf("%-7d", wholeNumber);
+                        System.out.print("|");
+                    System.out.printf("%-10d", wholeNumber * wholeNumber);
+                        System.out.print("|");
+                     System.out.printf("%-9d\n", wholeNumber * wholeNumber * wholeNumber);
+                 }
 
             System.out.println("Do you want to keep going?");
-            userAnswer = sc.next();
+            String userAnswer = sc.next();
+
             if (!userAnswer.equalsIgnoreCase("y")) {
-                userAnswer = false;
+                userContinues = false;
             }
-        }while (userAnswer);
-//
-//
-//        }
-   }
+        }   while (userContinues);
+    }
 }
+
+
