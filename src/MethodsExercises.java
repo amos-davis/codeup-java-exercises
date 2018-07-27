af8703c1e81b0cb4f1572bbea360748da953d7a8
Amos Davis <amos.davis367@gmail.com>
2018-07-27 11:51:44 -0500

    All exercises completed and uncommented

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -6,19 +6,21 @@ public class MethodsExercises {
 
     public static void main(String[] args) {
 
-//        System.out.println(addition(3,5));
-//        System.out.println(subtraction(1000, 900));
-//        System.out.println(multiplication(12,11));
-//        System.out.println(division(500,25));
-//
-//        System.out.println(modulus(12,5));
+        System.out.println(addition(3,5));
+        System.out.println(subtraction(1000, 900));
+        System.out.println(multiplication(12,11));
+        System.out.println(division(500,25));
+
+        System.out.println(modulus(12,5));
 
         System.out.println(getInteger(1, 10));
 
-//        System.out.print(calculateFactorials(3));
 
-//        System.out.println("Welcome to the dice roll game");
-//        playGame();
+        runFactorial();
+
+
+        System.out.println("Welcome to the dice roll game");
+        playGame();
     }
 
 //   -----Methods Exercise Number 1:
@@ -62,33 +64,54 @@ public class MethodsExercises {
     }
 
 //         --------Methods Exercise Number 3:------------
+    public static void runFactorial() {
+        Scanner sc = new Scanner(System.in);
+        boolean keepGoing = true;
 
-//    public static int calculateFactorials(int num) {
-//        Scanner sc = new Scanner(System.in);
-//        boolean keepGoing = true;
-//        int userInput = 0;
-//
+        do {
+            System.out.println(factorialDisplay());
+            System.out.println("Do you want to continue?");
+            String userAnswer = sc.next();
+            if(!userAnswer.equalsIgnoreCase("y")) {
+                keepGoing = false;
+            }
+        } while (keepGoing);
+    }
+
+    public static long factorialDisplay() {
+            Scanner sc = new Scanner(System.in);
+//            boolean keepGoing = true;
+//            sc.nextInt();
+
 //        do {
-//            System.out.print("Give me a number between 1 and 10: ");
-//            userInput = sc.nextInt();
-//
-//            if (userInput > 1 && userInput < 10) {
+            System.out.print("Give me a number between 1 and 10: ");
+            int userEntry = sc.nextInt();
+//            sc.nextInt(;
+            long calc = calculateFactorial(userEntry);
+        return calc;
+//            if(userEntry >= 1 && userEntry <= 10) {
+//                return calc;
+//            } else {
 //                keepGoing = false;
-//
-//                for (int i = userInput; i <= 1; i--) {
-//                    System.out.println(factorial *= 1);
-//                }
-//                return userInput;
+//                System.out.println("Not a valid entry.");
 //            }
-//        } while (keepGoing);
-//
-//
-//    pubic static int factorial(int num) {
-//        if (num <= 1)
-//            return 1;
-//        return num * factorial(num - 1);
-//}
 
+//        } while (keepGoing);
+//      return sc.nextInt();
+    }
+
+
+    public static int calculateFactorial(int userInput) {
+        int i;
+        int factorial = 1;
+        for (i = 1; i <= userInput; i++) {
+            factorial = factorial * i;
+        }
+        return factorial;
+    }
+
+//}
+// }
 //    --------Methods Exercise Number 4:-------
     public static void playGame() {
         Scanner sc = new Scanner(System.in);
@@ -103,16 +126,11 @@ public class MethodsExercises {
         System.out.println("Total is: " + (dieOne + dieTwo));
 
     }
-
+//
     public static int rollDice(int numberOfSides) {
         int random = (int)(Math.random() * numberOfSides + 1);
         return random;
     }
-
-
-
-
-
 
 
 }
