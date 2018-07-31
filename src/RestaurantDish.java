0104e01a7b0f7ee597171a8c16fb1a61b742b42f
Amos Davis <amos.davis367@gmail.com>
2018-07-31 11:04:06 -0500

    completed practice exercises for java Oop

---
+++ src/RestaurantDish.java
@@ -0,0 +1,44 @@
+public class RestaurantDish {
+
+    private int costInCents;
+    private String nameOfDish;
+    private boolean wouldRecommend;
+    private String dishSize;
+    private boolean goodAtmospher;
+
+
+//    public RestaurantDish(int costInCents, boolean wouldRecommend) {
+//        this.costInCents = costInCents;
+//        this.wouldRecommend = wouldRecommend;
+    }
+
+//regular method
+//    public String eat() {
+//        return "Nom nom nom!";
+//    }
+//
+//   public int getCostInCents () {
+//        return costInCents;
+//   }
+//
+//
+//    public String getNameOfDish() {
+//        return nameOfDish;
+//    }
+//
+//    public boolean isWouldRecommend() {
+//        return wouldRecommend;
+//    }
+//
+//    public void setCostInCents(int costInCents) {
+//        this.costInCents = costInCents;
+//    }
+//
+//    public void setNameOfDish(String nameOfDish) {
+//        this.nameOfDish = nameOfDish;
+//    }
+//
+//    public void setWouldRecommend(boolean wouldRecommend) {
+//        this.wouldRecommend = wouldRecommend;
+//    }
+//}
