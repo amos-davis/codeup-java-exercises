40981d6578ab4550de46e93785ff0ff5ea43e0e4
Amos Davis <amos.davis367@gmail.com>
2018-07-31 23:38:48 -0500

    Movie exercise for arrays completed

---
+++ src/BurgerTools.java
@@ -0,0 +1,16 @@
+public class BurgerTools {
+
+    public static String mostPopularTopping;
+    public static int averageDaysBeforeExpiration;
+    public static int temperatureWhenCooked;
+
+
+    public void grill() {
+        System.out.println(" I am grilling your burger now...");
+    }
+
+}
+
+
+
+
