0c7653f301c700f1be843697f8bcf7439173a8ff
Amos Davis <amos.davis367@gmail.com>
2018-08-02 16:54:43 -0500

    abstract/interface exercise complete-Go back over

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,28 +1,85 @@
-package Shapes;
+package shapes;
 
-public class Rectangle {
-
-    protected double length;
-    protected double width;
-
-    public Rectangle() {
-
-    }
+public class Rectangle extends Quadrilateral implements Measurable{
 
     public Rectangle(double length, double width) {
+//        calls the super class constructor
+        super(length, width);
+    }
+
+    @Override
+    public void setLength(double length) {
         this.length = length;
+    }
+
+    @Override
+    public void setWidth(double width) {
         this.width = width;
     }
 
-    public double getArea() {
-        return length * width;
-
-    }
-
+    @Override
     public double getPerimeter() {
         return (2 * length) + (2 * width);
-
     }
 
-
+    @Override
+    public double getArea() {
+        return length * width;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//----------------Ignore the code below for the above exercise on interface and abstract classes---------
+
+//package shapes;
+//
+//public class Rectangle {
+//
+//    protected double length;
+//    protected double width;
+//
+//    public Rectangle() {
+//
+//    }
+//
+//    public Rectangle(double length, double width) {
+//        this.length = length;
+//        this.width = width;
+//    }
+//
+//    public double getArea() {
+//        return length * width;
+//
+//    }
+//
+//    public double getPerimeter() {
+//        return (2 * length) + (2 * width);
+//
+//    }
+//
+//
+//}
