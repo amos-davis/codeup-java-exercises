eb5429afd1bb50c224590c2ca68e62aa56a72133
Amos Davis <amos.davis367@gmail.com>
2018-08-01 21:54:40 -0500

    Object Oriented Programming class exercise adjusted to oaccept int and boolean value. Now completed

--- src/RestaurantDish.java
+++ src/RestaurantDish.java
@@ -6,39 +6,38 @@ public class RestaurantDish {
     private String dishSize;
     private boolean goodAtmospher;
 
-
-//    public RestaurantDish(int costInCents, boolean wouldRecommend) {
-//        this.costInCents = costInCents;
-//        this.wouldRecommend = wouldRecommend;
+   public RestaurantDish(int costInCents, boolean wouldRecommend) {
+        this.costInCents = costInCents;
+        this.wouldRecommend = wouldRecommend;
     }
 
 //regular method
-//    public String eat() {
-//        return "Nom nom nom!";
-//    }
+    public String eat() {
+        return "Nom nom nom!";
+    }
 //
-//   public int getCostInCents () {
-//        return costInCents;
-//   }
+   public int getCostInCents () {
+        return costInCents;
+   }
 //
 //
-//    public String getNameOfDish() {
-//        return nameOfDish;
-//    }
+    public String getNameOfDish() {
+        return nameOfDish;
+    }
 //
-//    public boolean isWouldRecommend() {
-//        return wouldRecommend;
-//    }
+    public boolean isWouldRecommend() {
+        return wouldRecommend;
+    }
 //
-//    public void setCostInCents(int costInCents) {
-//        this.costInCents = costInCents;
-//    }
+    public void setCostInCents(int costInCents) {
+        this.costInCents = costInCents;
+    }
 //
-//    public void setNameOfDish(String nameOfDish) {
-//        this.nameOfDish = nameOfDish;
-//    }
+    public void setNameOfDish(String nameOfDish) {
+        this.nameOfDish = nameOfDish;
+    }
 //
-//    public void setWouldRecommend(boolean wouldRecommend) {
-//        this.wouldRecommend = wouldRecommend;
-//    }
-//}
+    public void setWouldRecommend(boolean wouldRecommend) {
+        this.wouldRecommend = wouldRecommend;
+    }
+}
